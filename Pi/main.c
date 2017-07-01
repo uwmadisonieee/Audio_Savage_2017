82202342acfe6ad13a6b5dcb239c684e592f1a2a
sjfricke <sjfricke@wisc.edu>
2017-06-30 23:15:35 -0700

    demo probes temp and gets key

--- Pi/main.c
+++ Pi/main.c
@@ -1,8 +1,6 @@
 #include "server/server.h" 
-
-void onResponse( char* response ) {
-  printf("CALLBACK: %s\n", response);
-}
+#include <stdio.h>
+#include <unistd.h> // for sleep()
 
 void onKeyPress( char key ) {
   printf("Key Pressed: %c\n", key);
@@ -13,13 +11,12 @@ int main ( int argc, char* argv[] ) {
   int status;
   server_t server_config;
   char* data;
+  FILE* temperature_file;
+  double current_temperature;
   
   // set port of server
   server_config.port = 8000;
   
-  // set function to receive actions
-  server_config.response = onResponse;
-
   // set function to receive a key press
   server_config.onKeyPress = onKeyPress;
 
@@ -30,10 +27,20 @@ int main ( int argc, char* argv[] ) {
     printf("http Server didn't start up correctly\n");
     exit(1);
   }
-  
+
+  // Get temperature file from linux file system
+  temperature_file = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+  if (NULL == temperature_file) {
+    printf("No temperature file found on machine\n");
+    exit(1);
+  }
+
+  printf("Probing temperature and broadcasting to sockets\n");
   while(1){
-    printf("What is your biggest secret ever\n");
-    scanf("%s", data);
-    broadcast(data);    
+    fscanf(temperature_file, "%lf", &current_temperature);
+    current_temperature /= 1000;
+    broadcast(current_temperature);
+    rewind(temperature_file);
+    sleep(1); // waits 1 second before probing again
   }
 }
